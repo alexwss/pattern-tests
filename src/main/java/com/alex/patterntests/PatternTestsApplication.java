29bb3f186a806cc744e5409345c3c45d3c241ac6
alex santos <alexwssilva@gmail.com>
2021-03-22 22:24:19 -0300

    first commit

---
+++ src/main/java/com/alex/patterntests/PatternTestsApplication.java
@@ -0,0 +1,13 @@
+package com.alex.patterntests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/patterntests/PatternTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.patterntests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
